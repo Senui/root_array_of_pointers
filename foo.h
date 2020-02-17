679dd7dbb1c8e43077ef00b634f35595470a1148
Ahmad Hesam <hesam212@gmail.com>
2020-02-17 15:31:03 +0100

    Simple reproducer with only std::array

--- foo.h
+++ foo.h
@@ -1,16 +1,9 @@
 #ifndef FOO_H_
 #define FOO_H_
 
-#include <iostream>
 #include <array>
-#include <vector>
 
-#include "TBuffer.h"
 #include <Rtypes.h>
-
-#include "inline_vector.h"
-
-#define ARRAY
 
 class Base {
 public:
@@ -28,34 +21,9 @@ public:
   Foo(int foo) : foo_(foo) {}
   virtual ~Foo() {}
 
-  int GetA() { return foo_; }
-
 private:
   int foo_ = 0;
   ClassDefOverride(Foo, 1);
 };
 
-class Bar {
-  public:
-  Bar() {}
-#ifdef INLINE_VECTOR
-  Bar(const InlineVector<Base*, 2>& v) : bar_(v) {}
-#elif defined(ARRAY)
-  Bar(const std::array<Base*, 2>& v) : bar_(v) {}
-#else
-  Bar(const std::vector<Base*>& v) : bar_(v) {}
-#endif
-  virtual ~Bar() {}
-
-private:
-#ifdef INLINE_VECTOR
-  InlineVector<Base*, 2> bar_;
-#elif defined(ARRAY)
-  std::array<Base*, 2> bar_;
-#else
-  std::vector<Base*> bar_;
-#endif
-  ClassDef(Bar, 1);
-};
-
-#endif // FOO_H_
+#endif  // FOO_H_
--- main.cc
+++ main.cc
@@ -1,52 +1,29 @@
 #include "foo.h"
 
-#include "inline_vector.h"
-
-#include "TBufferJSON.h"
 #include <TFile.h>
-#include <iostream>
 
 #include <array>
-#include <vector>
+#include <iostream>
 
-#ifdef INLINE_VECTOR
-static constexpr char const *kJsonFile = "foo_ilv.json";
-#elif defined(ARRAY)
-static constexpr char const *kJsonFile = "foo_array.json";
-#else
-static constexpr char const *kJsonFile = "foo_vector.json";
-#endif
 static constexpr char const *kFile = "foo.root";
 static constexpr char const *kObjName = "foo";
 
 int main() {
-#ifdef INLINE_VECTOR
-  InlineVector<Base *, 2> ilv;
-#elif defined(ARRAY)
-  std::array<Base*, 2> ilv;
-#else
-  std::vector<Base *> ilv;
-  ilv.resize(2);
-#endif
-  Foo *f1 = new Foo(42);
-  Foo *f2 = new Foo(43);
-  ilv[0] = f1;
-  ilv[1] = f2;
-
-  Bar b(ilv);
-  TBufferJSON::ExportToFile(kJsonFile, &b, b.Class());
+  std::array<Base*, 2> arr;
+  arr[0] = new Foo(42);
+  arr[1] = new Foo(43);
 
   TFile f(kFile, "RECREATE");
-  f.WriteObject(&b, kObjName);
+  f.WriteObject(&arr, kObjName);
   f.Close();
 
   TFile ff(kFile);
-  Bar *b2 = nullptr;
-  ff.GetObject(kObjName, b2);
+  std::array<Base*, 2> *arr2 = nullptr;
+  ff.GetObject(kObjName, arr2);
 
-  // bool valid = dynamic_cast<Foo*>((*b2)[0]) != nullptr;
-  // std::cout << valid << std::endl;
+  std::cout << arr2 << std::endl;
+  bool valid = dynamic_cast<Foo*>((*arr2)[0]) != nullptr;
+  std::cout << valid << std::endl;
 
-  std::cout << b2 << std::endl;
   return 0;
 }
