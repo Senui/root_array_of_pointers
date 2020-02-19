5fe6cf5e14a5bbf03d6098309a18e3681caec964
Ahmad Hesam <hesam212@gmail.com>
2020-02-19 17:08:38 +0100

    Custom streamer for InlineVector

--- inline_vector.h
+++ inline_vector.h
@@ -1,28 +1,73 @@
 #ifndef INLINE_VECTOR_H_
 #define INLINE_VECTOR_H_
 
-#include <algorithm>
 #include <array>
-#include <cassert>
-#include <limits>
-#include <sstream>
-#include <vector>
+#include <iostream>
+#include <fstream>
+
+#include "Rtypes.h"
+#include "TBuffer.h"
+
+template <typename T>
+class VectorWrapper {
+public:
+  VectorWrapper() {}
+
+  std::vector<T> vector_;
+  ClassDefNV(VectorWrapper, 1);
+};
 
 template <typename T, uint16_t N>
 class InlineVector {
 public:
-  explicit InlineVector(TRootIOCtor *io_ctor) {} // Constructor for ROOT I/O
   InlineVector() {}
 
-  virtual ~InlineVector() {}
+  explicit InlineVector(TRootIOCtor *io_ctor) {} // Constructor for ROOT I/O
 
   T &operator[](uint16_t index) { return data_[index]; }
 
-private:
-  std::array<T, N> data_;
-  uint16_t size_ = 0;
+  void push_back(const T& element) {
+    data_[size_++] = element;
+  }
 
-  ClassDef(InlineVector, 1); // NOLINT
+private:
+  std::array<T, N> data_; //!
+  uint16_t size_ = 0;
+  ClassDefNV(InlineVector, 1);
 };
+
+template <typename T, uint16_t N>
+void InlineVector<T, N>::Streamer(TBuffer &R__b) {
+  std::ofstream file;
+  file.open("streamer.log");
+  if (R__b.IsReading()) {
+    file << "READING..." << std::endl;
+    VectorWrapper<T> vw;
+    R__b.ReadClassBuffer(VectorWrapper<T>::Class(), &vw);
+    R__b.ReadClassBuffer(InlineVector<T, N>::Class(), this);
+    // tranfer vw to this
+    file << vw.vector_ << std::endl;
+    for (auto *ve : vw.vector_) {
+      file << ve << std::endl;
+      file << ve->GetUniqueID() << std::endl;
+      this->push_back(ve);
+    }
+  } else {
+    // transfer data from this to f
+    file << "WRITING..." << std::endl;
+    VectorWrapper<T> vw;
+    vw.vector_.resize(this->size_);
+    for (uint16_t i = 0; i < this->size_; i++) {
+      vw.vector_[i] = this->data_[i];
+    }
+    R__b.WriteClassBuffer(VectorWrapper<T>::Class(), &vw);
+    R__b.WriteClassBuffer(InlineVector<T, N>::Class(), this);
+  }
+}
+
+#ifdef __ROOTCLING__
+#include "TObject.h"
+static InlineVector<TObject *, 2> iv3;
+#endif
 
 #endif // INLINE_VECTOR_H_
--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
-#include "foo.h"
+#include "inline_vector.h"
 
 #include <TFile.h>
+#include <TObject.h>
 
 #include <array>
 #include <iostream>
@@ -9,21 +10,24 @@ static constexpr char const *kFile = "foo.root";
 static constexpr char const *kObjName = "foo";
 
 int main() {
-  Foo foo;
-  foo.Create();
+  InlineVector<TObject*, 2> iv;
+
+  for (size_t i = 0; i < 2; i++) {
+    TObject* o = new TObject(); o->SetUniqueID(i);
+    iv[i] = o;
+  }
 
   TFile f(kFile, "RECREATE");
-  f.WriteObject(&foo, kObjName);
+  f.WriteObject(&iv, kObjName);
   f.Close();
 
   TFile ff(kFile);
-  // std::array<Foo*, 2> *arr2 = nullptr;
-  Foo *foo2 = nullptr;
-  ff.GetObject(kObjName, foo2);
+  InlineVector<TObject*, 2> *iv2 = nullptr;
+  ff.GetObject(kObjName, iv2);
 
-  std::cout << foo2 << std::endl;
-  // bool valid = dynamic_cast<Foo*>((*arr2)[0]) != nullptr;
-  // std::cout << valid << std::endl;
+  std::cout << "iv2 = " << iv2 << std::endl;
+  std::cout << "(*iv2)[1] = " << (*iv2)[1] << std::endl;
+  std::cout << "(*iv2)[1]->GetUniqueID() = " << (*iv2)[1]->GetUniqueID() << std::endl;
 
   return 0;
 }
